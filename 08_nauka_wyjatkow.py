7e9fa1f731775c34fb4f7b6f0a91bde6c0399199
grados73 <gradowski.kam@gmail.com>
2021-04-19 19:29:58 +0200

    dodana obsluga wyjatku od dzielenia przez 0

---
+++ 08_nauka_wyjatkow.py
@@ -0,0 +1,9 @@
+while(1):
+    liczba = int(input("Podaj liczbe przez ktora mam podzielic 50: "))
+    try:
+        wynik = 50.0 / liczba
+        print("50 podzielone przez " + str(liczba) + " wynosi " + str(wynik) + "\n")
+    except ZeroDivisionError:
+        print("Nie dziel przez Zero! \n")
+    if liczba == 777:
+        break
