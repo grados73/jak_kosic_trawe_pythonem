701b61c1e2f97c2325d73a2cef12cd02861f44fd
grados73 <gradowski.kam@gmail.com>
2021-04-27 19:31:39 +0200

    Pusty projekt pod program szukajacy maili i nr tel w schowku

--- 11_zabawa_z_wyrazeniami_reg.py
+++ 11_zabawa_z_wyrazeniami_reg.py
@@ -1,3 +1,4 @@
+## https://www.regexpal.com/
 import re
 phoneNumberRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d')
 mo = phoneNumberRegex.search("Moj numer telefonu to 608-110-658, kamil.")
---
+++ 12_szukaj_nr_tel_i_email_w_schowku.py
@@ -0,0 +1,2 @@
+#! python3
+## 12_szukaj_nr_tel_i_email_w_schowku.py
