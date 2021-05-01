ca5c03418ea17af32cad15ae5c99060b08c9d97e
grados73 <gradowski.kam@gmail.com>
2021-05-01 11:26:06 +0200

    Dodanie Regexów od dużych, małych liter i cyfr

--- 13_czy_haslo_jest_silne.py
+++ 13_czy_haslo_jest_silne.py
@@ -2,3 +2,11 @@
 
 import pyperclip
 import re
+
+## WYRAŻENIA REGULARNE
+### WYRAŻENIE REGULARNE SPRAWDZAJĄCE CZY ZAWIERA MAŁE LITERY
+maleLiteryRegex = re.compile(r'[a-z]+')
+### WYRAŻENIE REGULARNE SPRAWDZAJĄCE CZY ZAWIERA DUŻE LITERY
+duzeLiteryRegex = re.compile(r'[A-Z]+')
+### WYRAŻENIE REGULARNE SPRAWDZAJĄCE CZY ZAWIERA CYFRY
+cyfryRegex = re.compile(r'[0-9]+')
