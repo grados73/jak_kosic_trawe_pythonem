fcfe025607971c540f1bfe42c104874ea3200607
grados73 <gradowski.kam@gmail.com>
2021-05-01 11:41:25 +0200

    Działa - sprawdza siłę hasła znajdującego się w schowku

--- 13_czy_haslo_jest_silne.py
+++ 13_czy_haslo_jest_silne.py
@@ -10,3 +10,28 @@ maleLiteryRegex = re.compile(r'[a-z]+')
 duzeLiteryRegex = re.compile(r'[A-Z]+')
 ### WYRAŻENIE REGULARNE SPRAWDZAJĄCE CZY ZAWIERA CYFRY
 cyfryRegex = re.compile(r'[0-9]+')
+
+## SPRAWDZENIE SIŁY HASŁA ZNAJDUJĄCEGO SIĘ W SCHOWKU
+while(1):
+    text = str(pyperclip.paste()) ## wklejenie zawartości schowka do zmiennej text
+    if len(text) < 8:
+        print("Twoje hasło jest zbyt krótkie. Hasło musi zawierać przynajmniej 8 znaków!")
+        break
+
+    mo1 = maleLiteryRegex.search(text)
+    if  mo1 == None:
+        print("Twoje hasło jest słabe. Hasło musi zawierać małe litery!")
+        break
+
+    mo2 = duzeLiteryRegex.search(text)
+    if mo2 == None:
+        print("Twoje hasło jest słabe. Hasło musi zawierać duże litery!")
+        break
+
+    mo3 = cyfryRegex.search(text)
+    if mo3 == None:
+        print("Twoje hasło jest słabe. Hasło musi zawierać cyfry!")
+        break
+    print("Twoje hasło jest wystarczająco silne.")
+    break
+
