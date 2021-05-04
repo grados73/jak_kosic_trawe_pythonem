4b36ee9e77f6eca436fd68779eb13571e1dd3c8e
grados73 <gradowski.kam@gmail.com>
2021-05-04 20:31:19 +0200

    Dodanie nazw stanów wraz ze stolicami do ukłądania testów

---
+++ 14_generowanie_losowych_plikow_quizu.py
@@ -0,0 +1,12 @@
+#! python3
+
+import random
+
+capitals={'Alabama' : 'Montgomery', 'Alaska' : 'Juneau', 'Arizona' : 'Phoenix', 'Arkansas' : 'Little rock', 'Kalifornia' : 'Sacramento', 'Kolorado' : 'Denver', 'Connecticut' : 'Hartford',
+          'Delaver' : 'Dover', 'Floryda' : 'Tallahassee', 'Georgia' : 'Atlanta', 'Hawaje' : 'Honolulu', 'Idaho' : 'Boise', 'Illinois' : 'Springfield', 'Indiana' : 'Indianapolis',
+          'Iowa' : 'Des Moines', 'Kansas' : 'Topeka', 'Kentucky' : 'Frankford', 'Luizjana' : 'Baton Rouge', 'Maine' : 'Augusta', 'Maryland' : 'Annapolis', 'Massachusetts' : 'Boston',
+          'Michigan' : 'Lansing', 'Minesota' : 'Saint Paula', 'Mississippi' : 'Jackson', 'Missouri' : 'Jefferson City', 'Montana' : 'Helena', 'Nebrasca' : 'Lincoln', 'Nevada' : 'Carson City',
+          'New Hampshire' : 'Concord', 'New Jersey' : 'Trenton', 'Nowy Meksyk' : 'Santa Fe', 'New York' : 'Albany', 'Karolina Północna' : 'Releigh', 'Dakota północna': 'Bismark',
+          'Ohio' : 'Columbus', 'Oclahoma' : 'Oclahoma City', 'Oregon' : 'Salem', 'Pensylwania' : 'Harrisburg', 'Rhode Island' : 'Providence', 'Karolina Północna' : 'Columbia',
+          'Dakota Północna' : 'Pierre', 'Tenessee' : 'Nashville', 'Teksas' : 'Austin', 'Utah' : 'Salt Lake City', 'Vermont' : 'Montpelier', 'Wirginia' : 'Richmond', 'Waszyngton' : 'Olympia',
+          'Wirginia Zachodnia' : 'Charleston', 'Wisconsin' : 'Madison', 'Wyoming' : 'Cheyenne'}
