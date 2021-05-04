8cb5f95bf67e5237a77859d1f48900371e34274e
grados73 <gradowski.kam@gmail.com>
2021-05-04 20:47:19 +0200

    Dodanie procedury losowania pytania i losowania odpowiedzi złcyh

--- 14_generowanie_losowych_plikow_quizu.py
+++ 14_generowanie_losowych_plikow_quizu.py
@@ -2,6 +2,7 @@
 
 import random
 
+## Nazwy stanów (Klucze) oraz odpowiadające im stolice (wartości)
 capitals={'Alabama' : 'Montgomery', 'Alaska' : 'Juneau', 'Arizona' : 'Phoenix', 'Arkansas' : 'Little rock', 'Kalifornia' : 'Sacramento', 'Kolorado' : 'Denver', 'Connecticut' : 'Hartford',
           'Delaver' : 'Dover', 'Floryda' : 'Tallahassee', 'Georgia' : 'Atlanta', 'Hawaje' : 'Honolulu', 'Idaho' : 'Boise', 'Illinois' : 'Springfield', 'Indiana' : 'Indianapolis',
           'Iowa' : 'Des Moines', 'Kansas' : 'Topeka', 'Kentucky' : 'Frankford', 'Luizjana' : 'Baton Rouge', 'Maine' : 'Augusta', 'Maryland' : 'Annapolis', 'Massachusetts' : 'Boston',
@@ -10,3 +11,29 @@ capitals={'Alabama' : 'Montgomery', 'Alaska' : 'Juneau', 'Arizona' : 'Phoenix', 
           'Ohio' : 'Columbus', 'Oclahoma' : 'Oclahoma City', 'Oregon' : 'Salem', 'Pensylwania' : 'Harrisburg', 'Rhode Island' : 'Providence', 'Karolina Północna' : 'Columbia',
           'Dakota Północna' : 'Pierre', 'Tenessee' : 'Nashville', 'Teksas' : 'Austin', 'Utah' : 'Salt Lake City', 'Vermont' : 'Montpelier', 'Wirginia' : 'Richmond', 'Waszyngton' : 'Olympia',
           'Wirginia Zachodnia' : 'Charleston', 'Wisconsin' : 'Madison', 'Wyoming' : 'Cheyenne'}
+
+## Wygenerowanie 35 plików quizu
+for quizNum in range(10):
+    #Utworzenie plików z quizem oraz odp
+    quizFile = open('D:\\Python\\jak_kosic_trawe_pythonem\\quizy\\capitalsQuiz%s.txt' % (quizNum + 1), 'w')
+    answerKeyFile = open('D:\\Python\\jak_kosic_trawe_pythonem\\quizy\\capitalsQuiz_answers%s.txt' % (quizNum + 1), 'w')
+
+    ##Zapis Nagłówka quizu
+    quizFile.write('Imię i Nazwisko:\n\nData:\n\nGrupa:\n\n')
+    quizFile.write((' ' * 20) + 'Quiz ze stolic stanów nr %s' % (quizNum + 1))
+    quizFile.write('\n\n')
+
+    ##Losowe ustalenie kolejności stanów
+    states = list(capitals.keys())
+    random.shuffle(states)
+
+    ##Iteracja przez 50 stanów i utworzenie pytania dotyczącego każdego z nich
+    for questionNum in range(50):
+        ##Przygotowanie prawidłowych i błędnych odp
+        correctAnswer = capitals[states[questionNum]]
+        wrongAnswers = list(capitals.values())
+        del wrongAnswers[wrongAnswers.index(correctAnswer)]
+        wrongAnswers = random.sample(wrongAnswers, 3)
+        answerOptions = wrongAnswers + [correctAnswer]
+        random.shuffle(answerOptions)
+        
